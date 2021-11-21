50cef9b978c91f3169c32d8c608e1abb5141125f
Olegib <oleg.babs2000@gmail.com>
2021-11-21 20:26:56 +0300

    dz2

---
+++ dz2/dz2_1.cpp
@@ -0,0 +1,35 @@
+﻿// dz2_1.cpp 
+
+#include <iostream>
+
+int main()
+{
+    int x, y, z;
+    int i, j, k;
+
+    std::cout << "x = ";
+    std::cin >> x;
+    std::cout << "y = ";
+    std::cin >> y;
+    std::cout << "z = ";
+    std::cin >> z;
+
+    int*** v = new int** [x];
+    for (i = 0; i < x; i++)
+    {
+        v[i] = new int* [y];
+
+        for (j = 0; j < y; j++)
+        {
+            v[i][j] = new int[z];
+
+            for (k = 0; k < z; k++)
+            {
+                v[i][j][k] = i * j * k;
+            }
+        }
+    }
+
+    return 0;
+}
+
---
+++ dz2/dz2_3.cpp
@@ -0,0 +1,45 @@
+﻿// dz2_3.cpp 
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int n, i, k, T;
+    std::vector<int> v;
+
+    std::cout << "Amount: ";
+    std::cin >> n;
+
+    std::cout << "Fill:" << "\n";
+    for (i = 0; i < n; i++)
+    {
+        std::cin >> k;
+        v.push_back(k);
+    }
+
+    int l = 0, r = n, m;
+
+    std::cout << "Search for ";
+    std::cin >> T;
+
+    while (l <= r)
+    {
+        m = (l + r) / 2;
+
+        if (v[m] < T)
+            l = m + 1;
+        else if (v[m] > T)
+            r = m - 1;
+        else
+            break;
+    }
+
+    if (v[m] == T)
+        std::cout << "success";
+    else 
+        std::cout << "not found";
+
+    return 0;
+}
+
